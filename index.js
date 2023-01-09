9c30331b9a246956183806a505cadd505e3407f4
CuriousNaman <117699788+CuriousNaman@users.noreply.github.com>
2023-01-09 07:02:55 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const ejs = require('ejs');
+const { url } = require('inspector');
+
+mongoose.set('strictQuery',false);
+const app = express();
+app.set('view engine','ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+mongoose.connect("mongodb+srv://admin-naman:namansoni99@cluster0.nwubt4c.mongodb.net/submitDB", {useNewUrlParser: true, useUnifiedTopology: true});
+
+const submitschema = new mongoose.Schema({
+    name: String,
+    class: String,
+    RollNum: Number
+});
+const Submit = mongoose.model("Submit",submitschema);
+
+Submit.updateOne({name: "Naman"}, {$set: {RollNum: 15}})
+
+
+// Submit.deleteOne({name: "Naman"},function(err){
+// if(err){
+//     console.log(err)
+// }else{
+//     console.log("Successfully deleted")
+// }
+// });
+
+app.get("/",function(req,res){
+    res.sendFile(__dirname + "/index.html");
+});
+app.post("/",function(req,res){
+    res.send("Thank you for your response");
+   let total = {
+     name : req.body.Name,
+     claass : req.body.Class,
+     rollno : req.body.Rollno
+   };
+    const submit = new Submit({
+    name: total.name,
+    class: total.claass,
+    RollNum: total.rollno
+});
+submit.save();
+app.get("/:topic",function(req,res){
+    let topic = (req.params.topic);
+
+    if(total.name === topic){
+        res.send("Hello");
+    }else{
+        console.log("Doesn't exist");
+    }
+});
+});
+
+app.listen(3000, function(){
+    console.log("Server is running")
+  });
