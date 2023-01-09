3397a51a1fd36ad2838a3ba22914b3048997666b
CuriousNaman <117699788+CuriousNaman@users.noreply.github.com>
2023-01-09 07:24:36 +0530

    Add Procfile and update ports

--- index.js
+++ index.js
@@ -55,7 +55,12 @@ app.get("/:topic",function(req,res){
     }
 });
 });
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port);
 
-app.listen(3000, function(){
+app.listen(port, function(){
     console.log("Server is running")
   });
